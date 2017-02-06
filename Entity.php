376b74edc5a9681b5deb86a035d4fafc47e5feda
serg <ohqncp>
2017-02-06 07:38:41 +0300

    -- simple relations

--- Entity.php
+++ Entity.php
@@ -495,67 +495,6 @@ abstract class Entity implements \ArrayAccess
         return new Relations\HasMany($this, $relatedClass, $link);
     }
 
-    public function hasMany2($related, $foreignKey = null, $localKey = null)
-    {
-        $instance = $this->newRelatedInstance($related);
-
-        $foreignKey = $foreignKey ?: $this->getForeignKey();
-
-        $localKey = $localKey ?: $this->getKeyName();
-
-        return new HasMany(
-            $instance->newQuery(), $this, $instance->getTable().'.'.$foreignKey, $localKey
-        );
-    }
-
-
-    public function hasOne2($related, $foreignKey = null, $localKey = null)
-    {
-        $instance = $this->newRelatedInstance($related);
-
-        $foreignKey = $foreignKey ?: $this->getForeignKey();
-
-        $localKey = $localKey ?: $this->getKeyName();
-
-        return new HasOne($instance->newQuery(), $this, $instance->getTable().'.'.$foreignKey, $localKey);
-    }
-
-    public function belongsTo2($related, $foreignKey = null, $ownerKey = null, $relation = null)
-    {
-        // If no relation name was given, we will use this debug backtrace to extract
-        // the calling method's name and use that as the relationship name as most
-        // of the time this will be what we desire to use for the relationships.
-        if (is_null($relation)) {
-            $relation = $this->guessBelongsToRelation();
-        }
-
-        $instance = $this->newRelatedInstance($related);
-
-        // If no foreign key was supplied, we can use a backtrace to guess the proper
-        // foreign key name by using the name of the relationship function, which
-        // when combined with an "_id" should conventionally match the columns.
-        if (is_null($foreignKey)) {
-            $foreignKey = Str::snake($relation).'_'.$instance->getKeyName();
-        }
-
-        // Once we have the foreign key names, we'll just create a new Eloquent query
-        // for the related models and returns the relationship instance which will
-        // actually be responsible for retrieving and hydrating every relations.
-        $ownerKey = $ownerKey ?: $instance->getKeyName();
-
-        return new BelongsTo(
-            $instance->newQuery(), $this, $foreignKey, $ownerKey, $relation
-        );
-    }
-
-    protected function newRelatedInstance($class)
-    {
-        return tap(new $class, function ($instance) {
-            if (! $instance->getConnectionName()) {
-                $instance->setConnection($this->connection);
-            }
-        });
-    }
 
 
 
